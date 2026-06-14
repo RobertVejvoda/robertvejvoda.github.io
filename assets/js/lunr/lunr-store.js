97cebec5a0e13335ce8c0650e2c273378776fc0a
Robert Vejvoda <robert@vejvoda.net>
2026-06-14 16:37:39 +0200

    Publish static blog site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Some thoughts and experience from implementing domain driven design",
+        "excerpt":"A lot happened this and last year, so why not to share some thoughts and experience from implementing domain driven design on an existing monolithic application. Sounds like a nonsense already. Warning: it’s just a high level overview and probably will not make much sense without experiencing similar issues. About...","categories": ["dev"],
+        "tags": ["DDD","domain driven design"],
+        "url": "/dev/some-thoughts-and-experience-implementing-ddd",
+        "teaser": null
+      },{
+        "title": "Are your class constructors heavy?",
+        "excerpt":"One of OOP principles says, that every class should enter valid states only. The bigger class it is, the harder to do. Typically we use class constructors so put class into that valid state. But it’s uncomfortable to have dozen of parameters as it becomes disorganized and difficult to use....","categories": ["dev"],
+        "tags": ["c#","patterns"],
+        "url": "/dev/are-your-class-constructors-heavy",
+        "teaser": null
+      },{
+        "title": "Greetings to Camunda, Zeebe and Dapr!",
+        "excerpt":"It’s awesome to see how Dapr and Camunda workflow engine work well together. By using BPMN and DMN we completely remove business logic from the code and only expose APIs to bind calls from Dapr Zeebe binding. The project can be found on GitHub. Story - the business process Expecting...","categories": ["dev"],
+        "tags": ["dapr","camunda","zeebe"],
+        "url": "/dev/greetings-to-camunda-zeebe-dapr",
+        "teaser": null
+      },{
+        "title": "Another demo of Dapr and Zeebe (Camunda)",
+        "excerpt":"After some time I went back to Dapr (v10) and Zeebe engine used in Camunda Platform 8. Updated projects to .NET 7 and tried again what I learned before and realized, I have to look back and refresh my memories. Some are like trying from scratch again. I noticed few...","categories": ["dev"],
+        "tags": ["dapr","camunda","zeebe"],
+        "url": "/dev/dapr-zeebe-demo",
+        "teaser": null
+      },{
+        "title": "Customer scoring with Camunda synchronous workflow",
+        "excerpt":"Within this demo we’re going to see once again how Dapr and Camunda Platform 8 works well together and provide language agnostic approach. In this case with focus on synchronous workflow processing which can be triggered by REST calls. Workflow handles a request to score customer according to his/her history...","categories": ["dev"],
+        "tags": ["Camunda","Dapr"],
+        "url": "/dev/customer-scoring-with-camunda-workflow",
+        "teaser": null
+      },{
+        "title": "Using Camunda DMN tables in parallel processing",
+        "excerpt":"Last time I tackled simple DMN tables using Collect(Sum) policy in demo called Customer Scoring. Next demo shows how to use #Camunda DMN tables for parallel processing and FEEL expressions. No glue, no source code, all on Zeebe side. The use case fits fictive restaurant scenario for payment and calculates...","categories": ["dev"],
+        "tags": ["Camunda"],
+        "url": "/dev/using-camunda-dmn-tables-in-parallel-processing",
+        "teaser": null
+      }]
